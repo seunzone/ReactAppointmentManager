6b9e7bbba9dd89cdc0d18f320adc5e5fdc98cb43
seunzone <darealseun@gmail.com>
2017-11-29 14:42:59 +0100

    project version1 commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,36 @@
-const gulp = require('gulp'),
-    browserify = require('gulp-browserify'),
-    webserver = require('gulp-webserver');
-    
-const src = './process',
-    app = './builds/app';
-
-gulp.task('js', () => {
-  return gulp.src( src + '/js/app.js' )
-    .pipe(browserify({
-      transform: 'reactify',
-      debug: true
-    }))
-    .on('error',  (err) => {
-      console.error('Error!', err.message);
-    })
-    .pipe(gulp.dest(app + '/js'));
-});
-
-gulp.task('html', () => {
-  gulp.src( app + '/**/*.html');
-});
-
-gulp.task('css', () => {
-  gulp.src( app + '/css/*.css');
-});
-
-gulp.task('watch', () => {
-  gulp.watch( src + '/js/**/*.js', ['js']);
-  gulp.watch( app + '/css/**/*.css', ['css']);
-  gulp.watch([ app + '/**/*.html'], ['html']);
-});
-
-gulp.task('webserver', () => {
-  gulp.src( app + '/')
-    .pipe(webserver({
+newFunction();
+function newFunction() {
+  const gulp = require('gulp'), browserify = require('gulp-browserify'), webserver = require('gulp-webserver');
+  const src = './process', app = './builds/app';
+  gulp.task('js', () => {
+    return gulp.src(src + '/js/app.js')
+      .pipe(browserify({
+        transform: 'reactify',
+        debug: true
+      }))
+      .on('error', (err) => {
+        console.error('Error!', err.message);
+      })
+      .pipe(gulp.dest(app + '/js'));
+  });
+  gulp.task('html', () => {
+    gulp.src(app + '/**/*.html');
+  });
+  gulp.task('css', () => {
+    gulp.src(app + '/css/*.css');
+  });
+  gulp.task('watch', () => {
+    gulp.watch(src + '/js/**/*.js', ['js']);
+    gulp.watch(app + '/css/**/*.css', ['css']);
+    gulp.watch([app + '/**/*.html'], ['html']);
+  });
+  gulp.task('webserver', () => {
+    gulp.src(app + '/')
+      .pipe(webserver({
         livereload: true,
         open: true
-    }));
-});
+      }));
+  });
+  gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
+}
 
-gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
